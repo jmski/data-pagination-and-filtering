118574f542ef4730f36a011e3c39892cb3f598cd
Jon-Michael Gelua <geluajm@gmail.com>
2020-09-29 13:56:50 -0400

    functional program pre-refactored
    meets expectations

--- js/script.js
+++ js/script.js
@@ -10,15 +10,13 @@ FSJS Project 2 - Data Pagination and Filtering
  * - Jon
 */
 
-
-//global variables
-const studentList = document.querySelector( '.student-list' ); //Select UL element with class student-list
-
 // this function will create and insert/append the elements needed to display a "page" of nine students
 function showPage ( list,page ) {
   // create two variables which will represent the index for the first and last student on the page
   const startIndex = ( page * 9 ) - 9;
   const endIndex = ( page * 9 );
+
+  const studentList = document.querySelector( '.student-list' ); //Select UL element with class student-list
 
   // set innerHTML property to studentList to an empty string
   studentList.innerHTML = ' ';
@@ -33,7 +31,7 @@ function showPage ( list,page ) {
          let html = `
          <li class="student-item cf">
             <div class="student-details">
-               <img class="avatar" src="${student.picture.thumbnail}" alt="Profile Picture">
+               <img class="avatar" src="${student.picture.large}" alt="Profile Picture">
                <h3>${student.name.first} ${student.name.last}</h3>
                <span class="email">${student.email}</span>
             </div>
@@ -41,51 +39,33 @@ function showPage ( list,page ) {
                <span class="date">Joined ${student.registered.date}</span>
             </div>
          </li>`;
-
-        // insert into DOM
-       studentList.insertAdjacentHTML('beforeend', html );
+       studentList.insertAdjacentHTML('beforeend', html );       // insert into DOM
       }
    }
 }
 
-
 //This function will create and insert/append the elements needed for the pagination buttons
 function addPagination( list ) {
-
-  // create a variable to calculate the number of pages needed
-  let numOfPages = Math.ceil(list.length / 9);
-  // select the element with a class of `link-list` and assign it to a variable
-  const linkList = document.querySelector('.link-list');
-  // set the innerHTML property of the variable you just created to an empty string
-   linkList.innerHTML = ' ';
-  // loop over the number of pages needed
-  for (let i = 1; i <= numOfPages; i++) {
-    // create the elements needed to display the pagination button
-    const buttonLink = `<li><button type="button">${i}</button></li>`;
-    //insert into DOM
-    linkList.insertAdjacentHTML( 'beforeend', buttonLink );
+  let numOfPages = Math.ceil(list.length / 9);   // create a variable to calculate the number of pages needed
+  const linkList = document.querySelector('.link-list');   // select the element with a class of `link-list` and assign it to a variable
+   linkList.innerHTML = ' ';   // set the innerHTML property of the variable you just created to an empty string
+  for ( let i = 1; i <= numOfPages; i++ ) {   // loop over the number of pages needed
+    const button = `<li><button type="button">${i}</button></li>`;     // create the elements needed to display the pagination button
+    linkList.insertAdjacentHTML( 'beforeend', button );    //insert into DOM
   }
   // select first button and set its class to active
-  linkList.querySelector( 'button' ).className = 'active';
+  const firstButton = document.querySelector( '.link-list > li > button' );
+  firstButton.className = 'active';
 
   // create an event listener on the `link-list` element
-  linkList.addEventListener ( 'click', ( event ) => {
-    // if the click target is a button:
+  linkList.addEventListener ( 'click', ( event ) => { 
     if ( event.target.tagName === 'BUTTON' ) {
-      // select the first element with active class then set className to empty string
-      linkList.document.querySelector( '.active' );
-      // remove the "active" class from the previous button
-      linkList.className = ' ';
-      // add the active class to the clicked button
-      event.target.className = 'active';
-      // call the showPage function passing the `list` parameter and page to display as arguments
-      showPage(list, event.target.textContent);
-    }
-
+      linkList.querySelector('.active').className = ' '; //Select first element with active class then remove active class
+      event.target.className = 'active';       // add the active class to the clicked button
+      showPage(list, event.target.textContent);       // call the showPage function passing the `list` parameter and page to display as arguments
+     }
   });
-
 }
-
 // Call functions
 showPage(data,1);
 addPagination(data);
