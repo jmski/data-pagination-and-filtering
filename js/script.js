df38c4ec0d9a610a9169b5aa9f0998e0090bb6ca
Jon-Michael Gelua <geluajm@gmail.com>
2020-09-29 11:24:32 -0400

    updated part 1

--- js/script.js
+++ js/script.js
@@ -11,39 +11,44 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
+//global variables
+const studentList = document.querySelector(".student-list"); //Select UL element with class student-list
 
-const studentList = document.querySelector(".student-list");
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+// this function will create and insert/append the elements needed to display a "page" of nine students
 function showPage (list,page) {
-   const startIndex = ( page * 9 ) - 9;
-   const endIndex = ( page * 9 );
+  // create two variables which will represent the index for the first and last student on the page
+  const startIndex = ( page * 9 ) - 9;
+  const endIndex = ( page * 9 );
 
-   for ( let i = 0; i < list.length; i++) {
+// set innerHTML property to studentList to an empty string
+studentList.innerHTML = ' ';
 
+  // loop over the length of the `list` parameter
+  for ( let i = 0; i < list.length; i++) {
+  // 
       if ( i >= startIndex && i < endIndex ) {
          let student = list[i];
 
+         // create DOM elements to display using template literals
          let html = `
          <li class="student-item cf">
-         <div class="student-details">
-           <img class="avatar" src="${student.picture.large}" alt="Profile Picture">
-           <h3>${student.name.first} ${student.name.last}</h3>
-           <span class="email">${student.email}</span>
-         </div>
-         <div class="joined-details">
-           <span class="date">Joined ${student.registered.date}</span>
-         </div>
-       </li>`;
+            <div class="student-details">
+               <img class="avatar" src="${student.picture.thumbnail}" alt="Profile Picture">
+               <h3>${student.name.first} ${student.name.last}</h3>
+               <span class="email">${student.email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${student.registered.date}</span>
+            </div>
+         </li>`;
 
+        // insert into DOM
        studentList.insertAdjacentHTML("beforeend",html);
       }
    }
 }
 
+showPage(data,1);
 
 /*
 Create the `addPagination` function
